70b7f70829fc18c152c935ebc81699315a39e3ab
Kenneth Carter <kxccarter@gmail.com>
2020-07-27 13:16:17 -0500

    Builds speedControl and passengerList reducers in index.js

--- src/index.js
+++ src/index.js
@@ -2,17 +2,43 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import logger from 'redux-logger';
 
 // put your reducers here!
 
+const speedControl = (state = 0, action) => {
+  if (action.type === 'INCREASE') {
+    return state + 1;
+  } else if (action.type === 'DECREASE') {
+    if (state === 0) {
+      return state;
+    }
+    return state - 1;
+  }
+  return state;
+};
+
+const passengerList = (state = [], action) => {
+  if (action.type === 'ADD_PASSENGER') {
+    return [...state, action.payload];
+  }
+  return state;
+};
 
 // be sure to combine your reducers!
 const storeInstance = createStore(
-  // reducers,
+  {
+    speedControl,
+    passengerList,
+  },
   applyMiddleware(logger)
-)
+);
 
-ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={storeInstance}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
